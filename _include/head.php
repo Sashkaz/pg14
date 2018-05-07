e89f32d3d889a92eeaf0f6ad059b27299a680525
calexandru <a.cheltuitor.sashkaz@gmail.com>
2018-05-07 22:07:59 +0200

    Lagt till include mappen

---
+++ _include/head.php
@@ -0,0 +1,8 @@
+ <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Första sida</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="_assets/_css/main.css" />
+    <script src="_assets/_js/jquery-3.3.1.min.js"></script>
+</head>
