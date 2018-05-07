02368be4756e3abadc6964a39d2579462b8a42a5
calexandru <a.cheltuitor.sashkaz@gmail.com>
2018-05-07 23:30:26 +0200

    Lagt till mallet för att vissa olika gym och även lite css

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 	<?php include("_include/head.php"); ?>
 	<body>
 		<?php 
+			include("_include/activity-list.php");
+			
 			if(empty($_GET["register"]))
 				include("_include/form-login.php"); 
 			else 
