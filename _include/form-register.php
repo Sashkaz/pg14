e6df455c2796e0474a37ef7763cabdc530ad0ef9
Nelson <Nelsone_93@hotmail.com>
2018-05-24 11:33:21 +0200

    changed gender values and style

--- _include/form-register.php
+++ _include/form-register.php
@@ -8,40 +8,34 @@
 <hr>
 <div class="login-option">
     <h2 class="text-center">Eller</h2>
-    <form name="register" action="_process/process-register.php" target="_self" method="POST">
-        <table class="table-layout text-justify">
+    <form name="register" action="_process/process-register.php" target="_self" method="POST" class="main-form">
+        <table class="table-layout" width="100%">
             <tbody>
                 <tr>
-                    <td><label for="first-name">Förnamn:</label></td>
-                    <td><input type="text" name="first-name" /></td>
-                    <td><label for="last-name">Efternamn:</label></td>
-                    <td><input type="text" name="last-name" /></td>
+                    <td><input type="text" name="first-name" placeholder="Förnamn"/></td>
+                    <td><input type="text" name="last-name" placeholder="Efternamn"/></td>
                 </tr>
                 <tr>
-                    <td><label for="email">E-post:</label></td>
-                    <td colspan="3"><input type="text" name="email" /></td>
+                    <td colspan="2"><input type="text" name="email" placeholder="E-post"/></td>
                 </tr>
                 <tr>
-                    <td><label for="pwd">Lösenord:</label></td>
-                    <td><input type="password" name="pwd" /></td>
-                    <td><label for="re-pwd">Skriv om Lösenord:</label></td>
-                    <td><input type="password" name="re-pwd" /></td>
+                    <td><input type="password" name="pwd" placeholder="Lösenord"/></td>
+                    <td><input type="password" name="re-pwd" placeholder="Upprepa Lösenord"/></td>
                 </tr>
                 <tr>
-                    <td><label for="gender">Kön:</label></td>
-                        <td>
-                            <select name="gender">
-                                <option value="none">Välj</option>
-                                <option value="m">Kille</option>
-                                <option value="f">Tjej</option>
-                                <option value="o">Annat</option>
-                            </select> 
+                    <td>
+                        <select name="gender">
+                            <option value="none" disabled selected>Kön</option>
+                            <option value=1>>Kille</option>
+                            <option value=2>Tjej</option>
+                            <option value=0>Annat</option>
+                        </select> 
                     </td>
-                    <td><label for="year">Födelsedatum:</label></td>
                     <td>
                         <select name="year">
+                        <option value="none" disabled selected>År</option>
                             <?php   
-                                $x = 2018;
+                                $x = 2002;
                                 while($x > 1950){
                                     echo "<option value=$x>$x</option>";
                                     $x--;
@@ -49,15 +43,19 @@
                             ?>
                         </select> 
                         <select name="month">
-                            <?php   
+                        <option value="none" disabled selected>Månad</option>
+                            <?php
+                                 
                                 $x = 1;
                                 while($x <= 12){
-                                    echo "<option value=$x>$x</option>";
+                                    $monthName = date('F', mktime(0, 0, 0, $x, 10));  
+                                    echo "<option value=$x>$monthName</option>";
                                     $x++;
                                 }
                             ?>
                         </select> 
                         <select name="day">
+                        <option value="none" disabled selected>Dag</option>
                             <?php   
                                 $x = 1;
                                 while($x <= 31){
@@ -69,10 +67,8 @@
                     </td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td><a href="index.php" class="custom-button1">Tillbaka till login</a></td>
-                    <td class="text-align-r"><input class="custom-button1" type="submit" id="create-account" /></td>
+                    <td><a href="index.php" class="custom-button2">Tillbaka till login</a></td>
+                    <td class="text-align-r"><input class="custom-button2" type="submit" id="create-account" /></td>
                 </tr>
             </tbody>
         </table>
