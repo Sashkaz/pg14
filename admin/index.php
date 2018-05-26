f9228dd4dcf400c990fa616532383a2aaa388c00
Sashkaz <a.cheltuitor.sashkaz@gmail.com>
2018-05-26 13:23:51 +0200

    re-organized ajax, includes and html and changed index reflect changes

--- admin/index.php
+++ admin/index.php
@@ -2,13 +2,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php include("_include/head.php"); ?>
+    <?php include("_include/_html/head.php"); ?>
 </head>
 <body>
     <?php
         if(isset($_SESSION["adminID"]) && !empty($_SESSION["adminID"])){
             echo "<div class=admin-top-nav>";
-                include("_include/admin-top-nav.php");
+                include("_include/_html/admin-top-nav.php");
             echo "</div>";
         }
     ?>
@@ -16,20 +16,20 @@
         <?php 
             if(isset($_SESSION["adminID"]) && !empty($_SESSION["adminID"])){
                 if(isset($_GET["users"]) && !empty($_GET["users"])){
-                    include("_ajax/show-users.php");
+                    include("_include/show-users.php");
                 }else if(isset($_GET["cities"]) && !empty($_GET["cities"])){
-                    include("_ajax/show-locations.php");
+                    include("_include/show-locations.php");
                 }else if(isset($_GET["hashtags"]) && !empty($_GET["hashtags"])){
-                    include("_ajax/show-hashtags.php");
+                    include("_include/show-hashtags.php");
                 }else if(isset($_GET["admins"]) && !empty($_GET["admins"])){
-                    include("_ajax/show-admins.php");
+                    include("_include/show-admins.php");
                 }else{
-                    include("_ajax/show-users.php");
+                    include("_include/show-users.php");
                 }
                 
                 include("_assets/_js/main.js");
             }else{
-                include("_include/login.php");
+                include("_include/_html/login.php");
             }
         ?>
     </div>
