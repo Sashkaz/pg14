a1fe1eccef09ff99fd2fe8230a5cc4b61a0c08f3
Sashkaz <a.cheltuitor.sashkaz@gmail.com>
2018-05-27 23:51:18 +0200

    left bar gets content form database

--- _include/left-content.php
+++ _include/left-content.php
@@ -1,96 +1,59 @@
+<?php
+    include("_include/_models/db.php");
+    $db = new Database("localhost", "root", "", "projekt");
+?>
 <div class="nav-divider">
     <h3>Stad</h3>
     <ul class="nav-city-dropdown">
-        <li>
-            <input type="checkbox" name="nav-city" value="1">
-            <label for="nav-city">Linköping</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-city" value="2">
-            <label for="nav-city">Malmö</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-city" value="3">
-            <label for="nav-city">Uppsala</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-city" value="4">
-            <label for="nav-city">Stockholm</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-city" value="4">
-            <label for="nav-city">Göteborg</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-city" value="5">
-            <label for="nav-city">Kalmar</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-city" value="6">
-            <label for="nav-city">Karlstad</label>
-        </li>
+        <?php
+            if($req = $db->q("select * from city")){
+                while ($row = $req->fetch_assoc()) {
+                    echo "
+                        <li>
+                            <input type=checkbox name=nav-city value=$row[cityID]>
+                            <label for=nav-city>$row[name]</label>
+                        </li>";
+                }
+            }else{
+                echo false;
+            }
+        ?>
     </ul>
 </div>
 <div class="nav-divider">
     <h3>Gym</h3>
     <ul class="nav-location-dropdown">
-        <li>
-            <input type="checkbox" name="nav-location" value="1">
-            <label for="nav-location">Fitness 24/7</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-location" value="2">
-            <label for="nav-location">Wellness</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-location" value="3">
-            <label for="nav-location">Friskis&Svettis</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-location" value="4">
-            <label for="nav-location">SATS</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-location" value="5">
-            <label for="nav-location">Actic</label>
-        </li>
+        <?php
+            if($req = $db->q("select * from location group by cityID")){
+                while ($row = $req->fetch_assoc()) {
+                    echo "
+                        <li>
+                            <input type=checkbox name=nav-location value=$row[activityID]>
+                            <label for=nav-location>$row[name]</label>
+                        </li>";
+                }
+            }else{
+                echo false;
+            }
+        ?>
     </ul>
 </div>
 <div class="nav-divider">
     <h3>Hashtag</h3>
     <ul class="nav-tag-dropdown">
-        <li>
-            <input type="checkbox" name="nav-tag" value="1">
-            <label for="nav-tag">#Crossfit</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-tag" value="2">
-            <label for="nav-tag">#Styrketräning</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-tag" value="3">
-            <label for="nav-tag">#Cardio</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-tag" value="4">
-            <label for="nav-tag">#Zumba</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-tag" value="5">
-            <label for="nav-tag">#Löpträning</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-tag" value="6">
-            <label for="nav-tag">#Boxning</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-tag" value="7">
-            <label for="nav-tag">#Cykling</label>
-        </li>
-        <li>
-            <input type="checkbox" name="nav-tag" value="8">
-            <label for="nav-tag">#Styrkelyft</label>
-        </li>
+        <?php
+            if($req = $db->q("select * from hashtaglist")){
+                while ($row = $req->fetch_assoc()) {
+                    echo "
+                        <li>
+                            <input type=checkbox name=nav-tag value=$row[hashtagListID]>
+                            <label for=nav-tag>$row[name]</label>
+                        </li>";
+                }
+            }else{
+                echo false;
+            }
+        ?>
     </ul>
 </div>
 <div class="nav-divider">
@@ -102,6 +65,4 @@
         </li>
     </ul>
 </div>
-<ul>
-    <li><a href="?show-register-form=true" class="custom-button1" >Test Register Form</a></li>
-</ul>
+
