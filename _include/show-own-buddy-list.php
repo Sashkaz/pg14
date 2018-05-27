5fda3785cc0799ae5f8b4922c73c905e5457a0d0
Sashkaz <a.cheltuitor.sashkaz@gmail.com>
2018-05-27 22:26:53 +0200

    added input for search and styled it

--- _include/show-own-buddy-list.php
+++ _include/show-own-buddy-list.php
@@ -1,1 +1,43 @@
-Own buddy list
+<link rel="stylesheet" type="text/css" href="_include/show-users.css">
+<link rel="stylesheet" type="text/css" href="_include/show-own-buddy-list.css">
+<div class="filter-buddy">
+    <input name=""class="custom-input1">
+</div>
+<div class="friend-container">
+    <div class="prof-container">
+        <div class="user-pic"><img src="_assets/_img/150x150.jpeg" ></div>
+        <div class="user-info">
+            <div class="profile-name">Alexandru Cheltuitor</div>
+            <div class="profile-verified">(v1) (v2) (a)</div>
+            <div class="profile-tag">
+                <span class="activity-tag">#Core</span>, 
+                <span class="activity-tag">#StrengthTraining</span>, 
+                <span class="activity-tag">#Basketball</span>
+            </div>
+        </div>
+    </div>
+    <div class="prof-container">
+        <div class="user-pic"><img src="_assets/_img/150x150.jpeg" ></div>
+        <div class="user-info">
+            <div class="profile-name">Alexandru Cheltuitor</div>
+            <div class="profile-verified">(v1) (v2) (a)</div>
+            <div class="profile-tag">
+                <span class="activity-tag">#Core</span>, 
+                <span class="activity-tag">#StrengthTraining</span>, 
+                <span class="activity-tag">#Basketball</span>
+            </div>
+        </div>
+    </div>
+    <div class="prof-container">
+        <div class="user-pic"><img src="_assets/_img/150x150.jpeg" ></div>
+        <div class="user-info">
+            <div class="profile-name">Alexandru Cheltuitor</div>
+            <div class="profile-verified">(v1) (v2) (a)</div>
+            <div class="profile-tag">
+                <span class="activity-tag">#Core</span>, 
+                <span class="activity-tag">#StrengthTraining</span>, 
+                <span class="activity-tag">#Basketball</span>
+            </div>
+        </div>
+    </div>
+</div>
