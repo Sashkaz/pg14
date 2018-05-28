283c330de75ed9737a5c28129800e0ecdbf5df10
Nelson <Nelsone_93@hotmail.com>
2018-05-28 13:52:16 +0200

    started working with profile info gather

--- _include/show-own-profile.php
+++ _include/show-own-profile.php
@@ -1,1 +1,15 @@
-Own profile settings
+<?php
+if (isset($_GET['u']) )
+{
+    $targetUser = $_GET["u"];
+}
+else
+{}
+if(isset($_SESSION["uid"]) && !empty($_SESSION["uid"]))
+{
+    $curUser = $_SESSION["uid"];
+    if (isset($_GET['u']) )
+    {
+        $targetUser = $_GET["u"];
+
+?>
