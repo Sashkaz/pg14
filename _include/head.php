e7b0734a0ee9088accb82b0989ebe86a014dfd11
Sashkaz <a.cheltuitor.sashkaz@gmail.com>
2018-05-29 00:29:27 +0200

    updated link to user and added missing header line for the fonts

--- _include/head.php
+++ _include/head.php
@@ -4,3 +4,4 @@
 <title>TrainingBuddies</title>
 <link rel="stylesheet" type="text/css" href="_assets/_css/main.css">
 <script src="_assets/_js/jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
