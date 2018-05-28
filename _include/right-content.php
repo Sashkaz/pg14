33fc4e283c7c8f6ef65c8e2052207562d4b19ca6
Nelson <Nelsone_93@hotmail.com>
2018-05-28 13:38:02 +0200

    merged all stylesheets into main.css and adjusted some values to get a more consistent look

--- _include/right-content.php
+++ _include/right-content.php
@@ -1,17 +1,18 @@
-<link rel="stylesheet" type="text/css" href="_include/rightbar-css.css">
 <?php
     $db = new Database("localhost", "root", "", "projekt");
 ?>
-<ul class="right-bar-list">
-    <?php
-        if($req = $db->q("select * from location")){
-            while ($row = $req->fetch_assoc())
-            { echo    "<li class=right-bar-item><a class=right-bar-link href=# > $row[name] <br> <span style=font-size:10px;color:grey;font-style:italic>$row[address]</span> </a> </li>
-            ";}
-        }
-        else
-        {
-            echo false;
-        }
-      ?>
+<div id="activity-container">
+    <ul class="right-bar-list">
+        <?php
+            if($req = $db->q("select * from location")){
+                while ($row = $req->fetch_assoc())
+                { echo    "<a class=right-bar-link href=# ><li class=right-bar-item> $row[name] <br> <span class=right-bar-subtext>$row[address]</span></li></a>
+                ";}
+            }
+            else
+            {
+                echo false;
+            }
+        ?>
     </ul>
+</div>
