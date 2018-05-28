fdc9361206a51cb80856245115ba7faebc175f34
Nelson <Nelsone_93@hotmail.com>
2018-05-28 11:39:20 +0200

    started working on left-content-messages and fixed right bar display

--- _include/left-content-messages.php
+++ _include/left-content-messages.php
@@ -1,9 +1,47 @@
-<link rel="stylesheet" type="text/css" href="_include/messages-css.css">
-<div class="conversation-selector">
-    <div class="conversation-pic">
-    <img src="_assets/_img/150x150.jpeg"/>
-    </div>
-    <div class="conversation-name">
-        Tom Cruise
-    </div>
+<?php
+    include("_include/_models/db.php");
+    $db = new Database("localhost", "root", "", "projekt");
+?>
+<link rel="stylesheet" type="text/css" href="_include/left-messages-css.css">
+<div id="conversation-container">
+    <a href="?show-messages=true&u=869810001527">
+        <div class="conversation-selector">
+            <span class="conversation-pic">
+            <img src="_assets/_img/150x150.jpeg"/>
+            </span>
+            <span class="conversation-name">
+                Tom Cruise
+            </span>
+        </div>
+    </a>
+    <a href="?show-messages=true&u=869810001527">
+        <div class="conversation-selector">
+            <span class="conversation-pic">
+            <img src="_assets/_img/150x150.jpeg"/>
+            </span>
+            <span class="conversation-name">
+                Tom Hanks
+            </span>
+        </div>
+    </a>
+    <a href="?show-messages=true&u=869810001527">
+        <div class="conversation-selector">
+            <span class="conversation-pic">
+            <img src="_assets/_img/150x150.jpeg"/>
+            </span>
+            <span class="conversation-name">
+                Tom Hiddleston
+            </span>
+        </div>
+    </a>
+    <a href="?show-messages=true&u=869810001527">
+        <div class="conversation-selector">
+            <span class="conversation-pic">
+            <img src="_assets/_img/150x150.jpeg"/>
+            </span>
+            <span class="conversation-name">
+                Tom Sawyer
+            </span>
+        </div>
+    </a>
 </div>
