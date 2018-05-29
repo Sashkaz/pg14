0a9220bffdaa51774de63de0c00de089bdcff1f6
Nelson <Nelsone_93@hotmail.com>
2018-05-29 11:45:42 +0200

    adjusted buddy list and show users to show profile images

--- _include/buddy-list.php
+++ _include/buddy-list.php
@@ -18,10 +18,18 @@
     $req = $db->q($sql);
     if($req->num_rows > 0){
         while ($row = $req->fetch_assoc()) {
+            if ($row["profilePicURL"] != "null")
+            {
+                $userPictureSrc= "data:image/jpeg;base64,".base64_encode($row["profilePicURL"]);
+            }
+            else
+            {
+                $userPictureSrc= "_assets/_img/150x150.jpeg";
+            }
             if($row["userID"] != null){
                 echo "
                     <a class=prof-container href=?show-profile=true&u=$row[publicID]>
-                        <div class=user-pic><img src=_assets/_img/150x150.jpeg ></div>
+                        <div class=user-pic><img src=$userPictureSrc></div>
                         <div class=user-info>
                             <div class=profile-name>$row[firstName] $row[lastName]</div>
                             <div class=profile-verified>(v1) (v2) (a)</div>
--- _include/show-users.php
+++ _include/show-users.php
@@ -11,9 +11,17 @@
     if($req->num_rows > 0){
         while ($row = $req->fetch_assoc()) {
             if($row["userID"] != null){
+                if ($row["profilePicURL"] != "null")
+                {
+                    $userPictureSrc= "data:image/jpeg;base64,".base64_encode($row["profilePicURL"]);
+                }
+                else
+                {
+                    $userPictureSrc= "_assets/_img/150x150.jpeg";
+                }
                 echo "
                     <a class=prof-container href=?show-profile=true&u=$row[publicID]>
-                        <div class=user-pic><img src=_assets/_img/150x150.jpeg ></div>
+                        <div class=user-pic><img src=$userPictureSrc></div>
                         <div class=user-info>
                             <div class=profile-name>$row[firstName] $row[lastName]</div>
                             <div class=profile-verified>(v1) (v2) (a)</div>
