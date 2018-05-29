25c909823c2c9d34432b7da8f67c1b997fbcc3ec
Nelson <Nelsone_93@hotmail.com>
2018-05-29 11:31:20 +0200

    fixed left-side-content bug that occured when user logged out

--- _include/left-content.php
+++ _include/left-content.php
@@ -1,29 +1,34 @@
 <?php
     include("_include/_models/db.php");
     $db = new Database("localhost", "root", "", "projekt");
-    if(!isset($_SESSION["uid"]) && empty($_SESSION["uid"])){
+    if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
-?>
-<input type="hidden" id="uid" value="<?php echo $_SESSION["uid"];?>"/>
-<div class="nav-divider" id="main_checkbox">
-    <h3>Stad</h3>
-    <ul class="nav-city-dropdown">
-        <?php
-            if($req = $db->q("select * from city")){
-                while ($row = $req->fetch_assoc()) {
-                    echo "
-                        <li>
-                            <input type=checkbox name=city value=$row[cityID]>
-                            <label for=nav_city>$row[name]</label>
-                        </li>";
-                }
-            }else{
-                echo false;
-            }
+    else if(isset($_SESSION["uid"]) && !empty($_SESSION["uid"])){
         ?>
-    </ul>
-</div>
-<div id="rest-search"></div>
+        <input type="hidden" id="uid" value="<?php echo $_SESSION["uid"];?>"/>
+        <div class="nav-divider" id="main_checkbox">
+            <h3>Stad</h3>
+            <ul class="nav-city-dropdown">
+                <?php
+                    if($req = $db->q("select * from city")){
+                        while ($row = $req->fetch_assoc()) {
+                            echo "
+                                <li>
+                                    <input type=checkbox name=city value=$row[cityID]>
+                                    <label for=nav_city>$row[name]</label>
+                                </li>";
+                        }
+                    }else{
+                        echo false;
+                    }
+                ?>
+            </ul>
+        </div>
+        <div id="rest-search"></div>
+        <?php
+    }
+?>
+
 
 
