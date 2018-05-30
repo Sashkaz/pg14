17bff3ae1810e6c159470dc11cd863536a475143
calexandru <a.cheltuitor.sashkaz@gmail.com>
2018-05-30 09:18:53 +0200

    fixing language

---
+++ _assets/_lang/eng.php
@@ -0,0 +1,46 @@
+<?php
+    $lang = 
+    [
+        "eng" =>    [
+            "login-form" => [
+                "login-button" => "Login",
+                "placeholder" => [
+                    "email" => "Email",
+                    "pwd" => "password"
+                ]
+            ]
+            ,"reg-form"  =>  [
+                "fb-login" => "Login with Facebook",
+                "gmail-login" => "Login with Gmail",
+                "placeholder" => [
+                    "fname" => "First name",
+                    "lname" => "Last name",
+                    "pwd" => "Password",
+                    "repwd" => "Retype password",
+                    "email" => "Email",
+                    "gender" => [
+                        0 => "Male",
+                        1 => "Female",
+                        2 => "Other"
+                    ],
+                    "y" => "Year",
+                    "m" => "Month",
+                    "d" => "Day",
+                ],
+                "back-button" => "Back to login",
+                "register-button" => "Create account now!"
+            ],
+            "top-nav-buttons" => [
+                "show-users" => "Show Users",
+                "my-profile" => "My Profile",
+                "messages" => "Messages",
+                "buddy-list" => "Buddy List",
+                "logout-button" => "Logout",
+            ],
+            "left-bar-filter" => [
+                ""
+            ]
+        ]
+
+    ]
+?>
