d7719d2236db8259338b0c8f65a85013b5c551eb
Jesper Danielsson <32525842+D0dG3@users.noreply.github.com>
2018-05-31 12:10:45 +0200

    last minute

--- _include/right-content.php
+++ _include/right-content.php
@@ -1,6 +1,8 @@
 <div id="activity-container">
     <ul class="right-bar-list">
         <?php
+        if (isset($_SESSION["uid"])&&!empty($_SESSION["uid"]))
+        {
             if($req = $db->q("select * from location")){
                 while ($row = $req->fetch_assoc())
                 { echo    "<a class=right-bar-link href=_process/process-active.php?locationID=$row[activityID] ><li class=right-bar-item> $row[name] <br> <span class=right-bar-subtext>$row[address]</span></li></a>
@@ -10,6 +12,7 @@
             {
                 echo false;
             }
+          }
         ?>
     </ul>
 </div>
